b383e5c13ee586f4797692878992719ba16e3a91
prabhat135 <jainprabhat807@gmail.com>
2024-06-22 14:12:32 +0530

    episode-03

--- App.js
+++ App.js
@@ -1,30 +1,30 @@
-import React from "react";
-import  ReactDOM  from "react-dom/client";
+  import React from "react";
+import ReactDOM from "react-dom/client";
 
-// ReactElement(object) => (convert code into) => HTML(that browser understands)
+// JSX (transpiled before it reaches the JS engine) - PARCEL - Babel
+ 
+// React.createElement => ReactElement- JS Object => HTMLElement(render)
+// JSX => Babel transpiles it to React.createElement => ReactElement- JS Object => HTMLElement(render)
 
-// creating new element h1 i.e. core react thing so we using 'React.' here
-const heading = React.createElement(
-    "h1", 
-    {id: "heading" }, 
-    "Hello world from React!!"
-); 
-// {}: these contains attribute for h1 tag
+// React Element
+const Heading = () => (
+    <h1 className="heading" tabIndex="3">
+        This is Heading!!
+    </h1>
+);
 
-const parent = [React.createElement(
-    "div",
-    { id: "parent1"},
-    React.createElement(
-        "div",
-        { id: "child1"},
-        [React.createElement("h1", {}, "I am a H1 tag and you know that this text is generating automatically on saving."),
-        React.createElement("h2", {}, "I am a H2 tag.")]
-    )
-)];
+// React Functional Component - A JavaScript Function which returns a ReactElement.  
+
+const HeadingComponent = () => (
+    <h3>
+        {Heading()}
+        <Heading />
+        <Heading></Heading>
+        Namaste React Functional Component
+    </h3>
+);
 
 
-// creating root where all react code runs and DOM manipulations happen
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
-// this render method takes object input and convert it into html tag and put it into browser.
+root.render(<HeadingComponent />);
