5034705cb48ae62fc0d0efe21cfc5343f543b204
prabhat135 <jainprabhat807@gmail.com>
2024-06-26 08:59:06 +0530

    episode-04 starting basic food app

--- App.js
+++ App.js
@@ -1,30 +1,146 @@
-  import React from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 
-// JSX (transpiled before it reaches the JS engine) - PARCEL - Babel
- 
-// React.createElement => ReactElement- JS Object => HTMLElement(render)
-// JSX => Babel transpiles it to React.createElement => ReactElement- JS Object => HTMLElement(render)
+/*  FOOD APP STRUCTURE
 
-// React Element
-const Heading = () => (
-    <h1 className="heading" tabIndex="3">
-        This is Heading!!
-    </h1>
-);
+* Header
+* - Logo
+* - Nav-Items
+* Body
+* - Search
+* - Resturants Container
+* --- Resturant Card
+* Footer
+* - Copyright
+* - Links
+* - Address
+* - Contact
+*/
 
-// React Functional Component - A JavaScript Function which returns a ReactElement.  
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://logos-world.net/wp-content/uploads/2020/11/Zomato-Logo.png"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-const HeadingComponent = () => (
-    <h3>
-        {Heading()}
-        <Heading />
-        <Heading></Heading>
-        Namaste React Functional Component
-    </h3>
-);
+const tempData = [
+  {
+    name: "Pizza Hut",
+    cloudinaryImageId: "19b555f364f7c5e0db9e815efb078b35",
+    locality: "Shivpuri Road",
+    areaName: "Civil Lines",
+    costForTwo: "₹350 for two",
+    cuisines: ["Pizzas"],
+    avgRating: 4,
+    id: "721",
+    avgRatingString: "4.0",
+    deliveryTime: 21,
+  },
+  {
+    name: "Dominos",
+    cloudinaryImageId: "d0450ce1a6ba19ea60cd724471ed54a8",
+    locality: "Shivpuri Road",
+    areaName: "Civil Lines",
+    costForTwo: "₹350 for two",
+    cuisines: ["Burgers", "Hot Dog"],
+    avgRating: 4,
+    id: "722",
+    avgRatingString: "4.9",
+    deliveryTime: 23,
+  },
+  {
+    name: "Sandwich Hut",
+    cloudinaryImageId: "5116a385bac0548e06c33c08350fbf11",
+    locality: "Shivpuri Road",
+    areaName: "Civil Lines",
+    costForTwo: "₹350 for two",
+    cuisines: ["Sandwich", "Noodles", "French Fries"],
+    avgRating: 4,
+    id: "723",
+    avgRatingString: "3.6",
+    deliveryTime: 32,
+  },
+  {
+    name: "Pasta Hut",
+    cloudinaryImageId: "d0450ce1a6ba19ea60cd724471ed54a8",
+    locality: "Shivpuri Road",
+    areaName: "Civil Lines",
+    costForTwo: "₹350 for two",
+    cuisines: ["Pasta", "Chowmin"],
+    avgRating: 4,
+    id: "724",
+    avgRatingString: "4.4",
+    deliveryTime: 26,
+  },
+];
 
+
+const RestaurantCard = (props) => {
+  const { resData } = props;
+
+  const { name, cuisines, avgRatingString, deliveryTime, cloudinaryImageId } =
+    resData;
+
+  return (
+    <div className="restaurant-card">
+      <div className="restaurant-img">
+        <img
+          className="restaurant-image"
+          src={
+            "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+            cloudinaryImageId
+          }
+        />
+      </div>
+      <h2>{name}</h2>
+      <h3>{cuisines.join(", ")}</h3>
+      <h4>{avgRatingString} Stars</h4>
+      <h4>{deliveryTime} MINS</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search Restaurent</div>
+      <div className="restaurant-container">
+        {/* <RestaurantCard resData={tempData[0]} />
+        <RestaurantCard resData={tempData[1]} />
+        <RestaurantCard resData={tempData[2]} />
+        <RestaurantCard resData={tempData[3]} /> */}
+        {tempData.map((restaurant) => (
+          <RestaurantCard key={restaurant.id} resData={restaurant} />   //We can also pass index as key.
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="root">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
